3eb5bc015c4f0c9418821b2af6dc705062f7e71e
SAKAMOTO Kazunori <exkazuu@gmail.com>
2017-02-07 19:06:30 +0900

    Fix heroku settings for using TypeScript via WebPack.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    resources 'react_tutorial', only: ['index']
-  end
+  #if Rails.env.development?
+  resources 'react_tutorial', only: ['index']
+  #end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
