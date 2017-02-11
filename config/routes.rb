4f860a7d2cf546229a4929d40b5082ed04115105
Shinya Kato <k.shin220@gmail.com>
2017-02-11 05:00:22 +0000

    Generate Devise User Model #26
    ユーザーモデルを生成
    Confirmableにした
    他のカラムは後で追加する
    ビューとコントローラーは後でカスタマイズする予定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
-  #if Rails.env.development?
-  resources 'react_tutorial', only: ['index']
-  #end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations',
+    sessions:      'users/sessions',
+  }
+
+  #if Rails.env.development?
+  resources 'react_tutorial', only: ['index']
+  #end
 end
