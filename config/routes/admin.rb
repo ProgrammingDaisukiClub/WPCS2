a3e555687f2242e3468c12ec304dc68409dd553a
hikalium <hikalium@hikalium.com>
2017-02-14 19:20:55 +0900

    Fix

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-      devise_for :admins, controllers: {
-        confirmations: 'admin/admins/confirmations',
-        passwords:      'admin/admins/passwords',
-        registrations:  'admin/admins/registrations',
-        sessions:       'admin/admins/sessions',
-      }
+  scope :admin do
+    devise_for :admins, controllers: {
+      confirmations: 'admin/admins/confirmations',
+      passwords:     'admin/admins/passwords',
+      registrations: 'admin/admins/registrations',
+      sessions:      'admin/admins/sessions',
+    }
   end
 end
