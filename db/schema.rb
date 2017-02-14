596202cb17814f71484c674e395acd5421903487
hikalium <hikalium@hikalium.com>
2017-02-14 18:36:20 +0900

    Add confirmable for admin

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 20170214081523) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
