148b246a20f60a0f50688955a90fcd85dbeb4d01
hikalium <hikalium@hikalium.com>
2017-02-14 18:06:19 +0900

    Add views / controllers

--- config/routes/admin.rb
+++ config/routes/admin.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  scope :admin do
-    devise_for :admins
+  namespace :admin do
+      devise_for :admins, controllers: {
+        confirmations: 'admin/admins/confirmations',
+        passwords:      'admin/admins/passwords',
+        registrations:  'admin/admins/registrations',
+        sessions:       'admin/admins/sessions',
+      }
   end
 end
