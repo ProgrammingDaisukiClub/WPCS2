9b88558e5c3f0fc80817a2a2a729fb4eeeab15e5
Shinya Kato <k.shin220@gmail.com>
2017-02-17 10:17:03 +0000

    fix

--- src/tutorial/index.tsx
+++ src/tutorial/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import Hello from 'tutorial/Hello';
+import Hello from './Hello';
 
 ReactDOM.render(<Hello content='hello world'/>, document.getElementById('app'));
