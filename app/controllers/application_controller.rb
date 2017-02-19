f93e01bf618318c1055b21d867452a55f7e0aa8e
Shinya Kato <k.shin220@gmail.com>
2017-02-19 17:27:23 +0000

    Internationalization #8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_filter :set_locale
+
+  def set_locale
+    I18n.locale = params[:lang] || I18n.default_locale
+  end
 end
