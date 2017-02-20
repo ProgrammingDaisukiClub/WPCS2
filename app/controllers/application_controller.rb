3b381ae100bb01d666f97c7bb720059768b996ba
Shinya Kato <k.shin220@gmail.com>
2017-02-20 09:35:33 +0000

    fix #8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :set_locale
+  before_action :set_locale
 
   def set_locale
     I18n.locale = params[:lang] || I18n.default_locale
