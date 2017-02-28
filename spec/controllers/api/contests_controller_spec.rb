7decc58acf26732d0b56d14fbd51069cce028bfe
hikalium <hikalium@users.noreply.github.com>
2017-02-28 13:05:15 +0000

    Add tests for POST /api/contests/:id/entry

--- spec/controllers/api/contests_controller_spec.rb
+++ spec/controllers/api/contests_controller_spec.rb
@@ -3,15 +3,42 @@ require 'rails_helper'
 RSpec.describe Api::ContestsController, type: :controller do
   include Devise::Test::ControllerHelpers
 
-  describe 'GET #show' do
-    # GET /api/contents/:id
+  let(:params) do
+    {
+      id: contest.present? ? contest.id : Contest.pluck(:id).push(0).max.next,
+      lang: lang
+    }
+  end
 
-    let(:params) do
-      {
-        id: contest.present? ? contest.id : Contest.pluck(:id).push(0).max.next,
-        lang: lang
-      }
+  shared_examples 'return HTTP 201 Created' do
+    it 'return HTTP 201 Created' do
+      get :show, params: params
+      expect(response).to have_http_status 201
     end
+  end
+
+  shared_examples 'return HTTP 404 Not Found' do
+    it 'return HTTP 404 Not Found' do
+      get :show, params: params
+      expect(response).to have_http_status 404
+    end
+  end
+
+  shared_examples 'return HTTP 403 Forbidden' do
+    it 'return HTTP 403 Forbidden' do
+      get :show, params: params
+      expect(response).to have_http_status 403
+    end
+  end
+
+  shared_examples 'return HTTP 409 Conflict' do
+    it 'return HTTP 409 Conflict' do
+      get :show, params: params
+      expect(response).to have_http_status 409
+    end
+  end
+
+  describe 'GET /api/contents/:id' do
 
     let(:json_without_problems) do
       {
@@ -45,13 +72,6 @@ RSpec.describe Api::ContestsController, type: :controller do
       sign_in(user) if user
     end
 
-    shared_examples 'return http not found' do
-      it 'return http not found' do
-        get :show, params: params
-        expect(response).to have_http_status(:not_found)
-      end
-    end
-
     shared_examples 'return http success and json without problems' do
       it 'return http success' do
         get :show, params: params
@@ -81,13 +101,13 @@ RSpec.describe Api::ContestsController, type: :controller do
         context 'when the contest does not exist and the user does not login,' do
           let(:contest) { nil }
           let(:user)    { nil }
-          it_behaves_like 'return http not found'
+          it_behaves_like 'return HTTP 404 Not Found'
         end
 
         context 'when the contest does not exist and the user logins,' do
           let(:contest) { nil }
           let(:user)    { create(:user) }
-          it_behaves_like 'return http not found'
+          it_behaves_like 'return HTTP 404 Not Found'
         end
       end
 
@@ -151,9 +171,115 @@ RSpec.describe Api::ContestsController, type: :controller do
     end
   end
 
-  describe 'POST #entry' do
+  describe 'POST /api/contests/:id/entry' do
+    ['ja', 'en'].each do |language|
+      let(:lang) { language }
+
+      describe 'Case 1: contest NOT found' do
+        context 'contest NOT existed' do
+          let(:contest) { nil }
+
+          context 'NOT logged in' do
+            let(:user)    { nil }
+            it_behaves_like 'return HTTP 404 Not Found'
+          end
+
+          context 'logged in' do
+            let(:user)    { create(:user) }
+            it_behaves_like 'return HTTP 404 Not Found'
+          end
+        end
+      end
+
+      describe 'Case 2: NOT logged in or AFTER contest period' do
+        context 'NOT logged in' do
+          let(:user)    { nil }
+
+          context 'BEFORE contest period' do
+            let(:contest) { create(:contest_preparing) }
+
+            it_behaves_like 'return HTTP 403 Forbidden'
+          end
+
+          context 'IN contest period' do
+            let(:contest) { create(:contest_holding) }
+
+            it_behaves_like 'return HTTP 403 Forbidden'
+          end
+
+          context 'AFTER contest period' do
+            let(:contest) { create(:contest_ended) }
+
+            it_behaves_like 'return HTTP 403 Forbidden'
+          end
+        end
+        context 'logged in' do
+          let(:user)    { create(:user) }
+
+          context 'AFTER contest period' do
+            let(:contest) { create(:contest_ended) }
+
+            it_behaves_like 'return HTTP 403 Forbidden'
+          end
+        end
+
+      end
+
+      describe 'Case 3: Already participated' do
+        context 'logged in' do
+          let(:user)    { create(:user) }
+
+          context 'participated' do
+            before do
+              create(:contest_registration, user: user, contest_id: contest.id)
+            end
+
+            context 'BEFORE contest period' do
+              let(:contest) { create(:contest_preparing) }
+
+              it_behaves_like 'return HTTP 409 Conflict'
+            end
+
+            context 'logged in, participated,  IN contest period' do
+              let(:contest) { create(:contest_holding) }
+
+              it_behaves_like 'return HTTP 409 Conflict'
+            end
+
+            context 'logged in, participated,  AFTER contest period' do
+              let(:contest) { create(:contest_ended) }
+
+              it_behaves_like 'return HTTP 409 Conflict'
+            end
+          end
+        end
+      end
+      
+      describe 'Case 4: Success' do
+        context 'logged in' do
+          let(:user)    { create(:user) }
+
+          context 'NOT participated' do
+
+            context 'BEFORE contest period' do
+              let(:contest) { create(:contest_preparing) }
+
+              it_behaves_like 'return HTTP 201 Created'
+            end
+
+            context 'IN contest period' do
+              let(:contest) { create(:contest_holding) }
+
+              it_behaves_like 'return HTTP 201 Created'
+            end
+
+          end
+        end
+      end
+
+    end
   end
 
-  describe 'GET #ranking' do
+  describe 'GET /api/contests/:id/ranking' do
   end
 end
