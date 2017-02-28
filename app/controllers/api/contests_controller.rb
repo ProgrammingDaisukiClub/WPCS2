25f53916827e146a69976b63c2c859eac738fda8
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2017-03-01 06:45:52 +0900

    fix Case 1 to avoid ActiveRecord::RecordNotFound

--- app/controllers/api/contests_controller.rb
+++ app/controllers/api/contests_controller.rb
@@ -1,17 +1,17 @@
 class Api::ContestsController < ApplicationController
   def show
-    unless contest = Contest.find(params[:id])
+    unless contest = Contest.find_by_id(params[:id])
       render(json: {}, status: 404) && return
     end
 
-    isUserRegistered = contest.registered_by(user)
+    isUserRegistered = contest.registered_by(current_user)
     if !signed_in? || (!contest.ended? && !isUserRegistered) || (!contest.started? && isUserRegistered)
       render json: {
         id: params[:id],
         name: params[:lang] == 'ja' ? contest.name_ja : contest.name_en,
         description: params[:lang] == 'ja' ? contest.description_ja : contest.description_en,
         joined: isUserRegistered
-      }, status: 404
+      }, status: 404 and return
     end
   end
 
