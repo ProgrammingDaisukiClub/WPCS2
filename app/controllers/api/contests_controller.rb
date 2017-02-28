d03bba3efb71f29dd072fc24f434c41297dbb40c
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2017-02-28 22:08:40 +0900

    GET /api/contents/:id Case 2

--- app/controllers/api/contests_controller.rb
+++ app/controllers/api/contests_controller.rb
@@ -1,9 +1,17 @@
 class Api::ContestsController < ApplicationController
   def show
-    if !signed_in? && !Contest.exists?(id: params[:id])
-      render json: { error: '404 error' }, status: 404
-    else
-      render json: {}
+    if !Contest.exists?(id: params[:id])
+      return render json: { error: '404 error' }, status: 404
+    end
+    contest = Contest.find(params[:id])
+
+    if (!signed_in?) || (signed_in? && Time.now < contest.end_at && !ContestRegistration.find_by(user_id: user.id).present?)  || (signed_in? && Time.now < contest.start_at && ContestRegistration.find_by(user_id: user.id).present?)
+      render json: {
+        id: params[:id],
+        name: params[:lang] == 'ja' ? contest.name_ja : contest.name_en,
+        description: params[:lang] == 'ja' ? contest.description_ja : contest.description_en,
+        joined: ContestRegistration.find_by(user_id: user.id).present?
+      }, status: 404
     end
   end
 
