09672643ddd64e77824094fbac6797eb5bc1cda8
hikalium <hikalium@users.noreply.github.com>
2017-03-07 10:58:08 +0000

    Update registrations/new.html.erb for user.name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def set_locale
     I18n.locale = lang_param
@@ -14,4 +15,10 @@ class ApplicationController < ActionController::Base
   def lang_param
     params.fetch(:lang, I18n.default_locale.to_s)
   end
+
+private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
