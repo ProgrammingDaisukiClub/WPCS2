127a2ff5501f1100c7ea07a4f696d543c5e32ea1
hikalium <hikalium@users.noreply.github.com>
2017-03-13 14:35:14 +0000

    Create title page skelton

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+  def index
+    @top_contest = Contest.limit(2).map do |contest|
+      contest.name_and_description(I18n.locale)
+    end
+    @all_contest = Contest.all.map do |contest|
+      contest.name_and_description(I18n.locale)
+    end
+  end
 end
