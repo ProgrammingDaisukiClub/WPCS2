0c960699e3c56749b84fd21769b79555575f1fdc
Shinya Kato <k.shin220@gmail.com>
2017-03-15 17:41:06 +0000

    Fix specs #136

--- spec/controllers/admin/contests_controller_spec.rb
+++ spec/controllers/admin/contests_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Admin::ContestsController, type: :controller do
   describe 'GET #show' do
     context 'when the user does not login' do
       it 'redirects to another page' do
+        pending 'now basic authentication is used instead'
         get :index
         expect(response).to have_http_status(302)
       end
