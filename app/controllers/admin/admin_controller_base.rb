456386d6b4247a64ba1ebcd672623c8d00a1e111
Shinya Kato <k.shin220@gmail.com>
2017-03-15 17:43:43 +0000

    Fix lint error #136

--- app/controllers/admin/admin_controller_base.rb
+++ app/controllers/admin/admin_controller_base.rb
@@ -3,7 +3,7 @@ class Admin::AdminControllerBase < ApplicationController
 
   def admin_authentication
     # redirect_to new_admin_session_path unless admin_signed_in?
-    # TODO Adminユーザー認証に戻す
+    # TODO Use Devise authentication instead of basic auth
     authenticate_or_request_with_http_basic('Administration') do |username, password|
       username == ENV['admin_username'] && password == ENV['admin_password']
     end
