fc9af8a92dfbda07b1592925c708babe7cd920c0
Shinya Kato <k.shin220@gmail.com>
2017-03-15 12:18:22 +0000

    Change mail sender #118

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = '"WPCS2" <no-reply@wpcs.com>'
+  config.mailer_sender = 'exkazuu@nii.ac.jp'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'DeviseMailer'
