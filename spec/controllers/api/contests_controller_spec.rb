41a52e16d03464db45442808b1061e0dbf079476
Shinya Kato <k.shin220@gmail.com>
2017-03-30 13:24:05 +0000

    Fix describe #175

--- spec/controllers/api/contests_controller_spec.rb
+++ spec/controllers/api/contests_controller_spec.rb
@@ -435,7 +435,7 @@ RSpec.describe Api::ContestsController, type: :controller do
 
           context 'NOT logged in' do
             let(:user) { nil }
-            it 'returns 404 Not Found' do
+            it 'returns 200 Success' do
               expect(response).to have_http_status 200
             end
           end
