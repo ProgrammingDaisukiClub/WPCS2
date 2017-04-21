ebcb1ad4af66a5a59316df99032f6ebcf9da40d5
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-04-21 23:23:17 +0900

    create basic for staging

--- app/controllers/admin/admin_controller_base.rb
+++ app/controllers/admin/admin_controller_base.rb
@@ -4,8 +4,10 @@ class Admin::AdminControllerBase < ApplicationController
   def admin_authentication
     # redirect_to new_admin_session_path unless admin_signed_in?
     # TODO Use Devise authentication instead of basic auth
-    authenticate_or_request_with_http_basic('Administration') do |username, password|
-      username == ENV['admin_username'] && password == ENV['admin_password']
+    unless ENV['STAGING'] == "true"
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == ENV['admin_username'] && password == ENV['admin_password']
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_action :staging_authentication
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -20,5 +20,15 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def staging_authentication
+    # redirect_to new_admin_session_path unless admin_signed_in?
+    # TODO Use Devise authentication instead of basic auth
+    if ENV['STAGING'] == "true"
+      authenticate_or_request_with_http_basic('Administration') do |username, password|
+        username == ENV['admin_username'] && password == ENV['admin_password']
+      end
+    end
   end
 end
