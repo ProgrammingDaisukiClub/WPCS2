216443e15db9b298881f08c6b113424168b66f26
Shinya Kato <k.shin220@gmail.com>
2017-04-24 04:51:23 +0000

    Use if option of before_action #188

--- app/controllers/admin/admin_controller_base.rb
+++ app/controllers/admin/admin_controller_base.rb
@@ -1,10 +1,9 @@
 class Admin::AdminControllerBase < ApplicationController
-  before_action :admin_authentication
+  before_action :admin_authentication, if: -> { ENV['staging'] != 'true' }
 
   def admin_authentication
     # redirect_to new_admin_session_path unless admin_signed_in?
     # TODO Use Devise authentication instead of basic auth
-    return if ENV['STAGING'] == 'true'
     authenticate_or_request_with_http_basic('Administration') do |username, password|
       username == ENV['admin_username'] && password == ENV['admin_password']
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :staging_authentication
+  before_action :staging_authentication, if: -> { ENV['staging'] == 'true' }
   before_action :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -23,9 +23,6 @@ class ApplicationController < ActionController::Base
   end
 
   def staging_authentication
-    # redirect_to new_admin_session_path unless admin_signed_in?
-    # TODO Use Devise authentication instead of basic auth
-    return unless ENV['STAGING'] == 'true'
     authenticate_or_request_with_http_basic('Administration') do |username, password|
       username == ENV['admin_username'] && password == ENV['admin_password']
     end
