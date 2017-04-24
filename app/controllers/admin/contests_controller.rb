31718e1b970f6f4e2c59cd60cdbec06dd597a0a0
PochecoPachico <sasimi35@gmail.com>
2017-04-24 18:11:43 +0900

    modified to pass rubocop check

--- app/controllers/admin/contests_controller.rb
+++ app/controllers/admin/contests_controller.rb
@@ -22,7 +22,7 @@ class Admin::ContestsController < Admin::AdminControllerBase
   # GET /contests/1/submissions
   def submissions
     data_set_ids = Contest.find_by_id(params[:id]).data_sets.pluck(:id)
-    @submissions = Submission.where(:data_set_id => data_set_ids)
+    @submissions = Submission.where(data_set_id: data_set_ids)
   end
 
   # POST /contests
