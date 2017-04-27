003b1b579e274c6b187a12f035c304a6fb08f87e
Shinya Kato <k.shin220@gmail.com>
2017-04-27 07:14:28 +0000

    Modify assosiations #170

--- app/controllers/admin/submissions_controller.rb
+++ app/controllers/admin/submissions_controller.rb
@@ -1,9 +1,11 @@
 class Admin::SubmissionsController < ApplicationController
+  SUBMISSIONS_PER_PAGE = 30
+
   def index
-    data_set_ids = Contest.find_by_id(params[:contest_id]).data_sets.pluck(:id)
-    @submissions = Submission
-                   .where(data_set_id: data_set_ids)
-                   .page(params[:page]).per(30).order(created_at: :desc)
-                   .includes(:user, :data_set, :problem)
+    @contest = Contest.find_by_id(params[:contest_id])
+    @submissions = @contest.submissions
+                           .page(params[:page]).per(SUBMISSIONS_PER_PAGE)
+                           .reorder(created_at: :desc)
+                           .includes(:problem, :data_set, :user)
   end
 end
