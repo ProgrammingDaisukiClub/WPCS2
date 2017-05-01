f9cbfc020bea1d3feec5b31e0957ad78e2844d5d
hikalium <hikalium@users.noreply.github.com>
2017-05-01 15:34:21 +0000

    Add name field for update profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def staging_authentication
