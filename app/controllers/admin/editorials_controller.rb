c1407bef609a2ce69fbb96e2f637329865eff4f3
PochecoPachico <sasimi35@gmail.com>
2017-05-13 16:15:49 +0900

    adminからEditorialを新規登録するところまで

---
+++ app/controllers/admin/editorials_controller.rb
@@ -0,0 +1,71 @@
+class Admin::EditorialsController < Admin::ControllerBase
+  before_action :set_editorial, only: [:show, :edit, :update, :destroy]
+
+  # GET /editorials/1
+  # GET /editorials/1.json
+  def show; end
+
+  # GET /editorials/new
+  def new
+    @editorial = Editorial.new(params.permit(:contest_id))
+  end
+
+  # GET /editorials/1/edit
+  def edit; end
+
+  # POST /editorials
+  # POST /editorials.json
+  def create
+    @editorial = Editorial.new(editorial_params)
+    @contest = Contest.find(params[:contest_id])
+
+    respond_to do |format|
+      if @editorial.save
+        format.html { redirect_to admin_contest_path(@contest), notice: 'Editorial was successfully created.' }
+        format.json { render :show, status: :created, location: admin_contest_path(@contest) }
+      else
+        format.html { render :new }
+        format.json { render json: @editorial.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /editorials/1
+  # PATCH/PUT /editorials/1.json
+  def update
+    respond_to do |format|
+      if @editorial.update(editorial_params)
+        format.html { redirect_to [:admin, @editorial], notice: 'editorial was successfully updated.' }
+        format.json { render :show, status: :ok, location: [:admin, @editorial] }
+      else
+        format.html { render :edit }
+        format.json { render json: @editorial.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /editorials/1
+  # DELETE /editorials/1.json
+  def destroy
+    contest = @editorial.contest
+    @editorial.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_contest_url(contest), notice: 'Editorial was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_editorial
+    @editorial = Editorial.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def editorial_params
+    params.require(:editorial).permit(
+      :contest_id, :content
+    )
+  end
+end
---
+++ app/controllers/editorials_controller.rb
@@ -0,0 +1,19 @@
+class EditorialsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def update
+  end
+end
