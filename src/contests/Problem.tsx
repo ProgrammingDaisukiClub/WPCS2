c35ac1c664cf99e2f563f59bf6164b4a2c15cd9a
Shinya Kato <k.shin220@gmail.com>
2017-05-15 10:18:08 +0000

    Fix error #231

--- src/contests/Problem.tsx
+++ src/contests/Problem.tsx
@@ -103,7 +103,7 @@ export default class Problem extends React.Component<ProblemProps, ProblemState>
                             key={ dataSet.id }
                             className={ `problem--dataSetTab${ dataSet.id === this.state.dataSetTabId ? '__active' : '' }` }
                             onClick={ () => this.onTabClick(dataSet.id) }>
-                            <i class={ dataSet.id === this.state.dataSetTabId ? 'fa fa-check-square-o' : 'fa fa-square-o' } aria-hidden="true"></i>　{ dataSet.label }
+                            <i className={ dataSet.id === this.state.dataSetTabId ? 'fa fa-check-square-o' : 'fa fa-square-o' }></i> { dataSet.label }
                           </li>
                         )) }
                       </ul>
