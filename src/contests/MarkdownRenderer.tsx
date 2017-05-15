228842554563db499b332a78ed0d03fe94ffb659
Toshifumi Kiyono <hayabusatoshihumi@gmail.com>
2017-05-15 22:07:03 +0900

    write katex

--- src/contests/MarkdownRenderer.tsx
+++ src/contests/MarkdownRenderer.tsx
@@ -2,6 +2,11 @@ import * as marked from 'marked';
 import * as React from 'react';
 import * as renderHTML from 'react-render-html';
 
+interface Katex {
+  renderToString: any;
+}
+declare var katex: Katex;
+
 export interface Props {
   text: string;
 }
@@ -12,11 +17,19 @@ export default class MarkdownRenderer extends React.Component<Props, {}> {
   constructor(props: Props) {
     super(props);
     this.renderer = new marked.Renderer();
-    this.renderer.em = (str: string) => {
-      return '_' + str + '_';
+    this.renderer.code = (str: string) => {
+      return '<div>' + str.replace(/\n/g, "<br/>") + '</div>' ;
     };
   }
 
+  private renderKatexFragments(text: string): string {
+    const items = text.split("$");
+    let result = "";
+    for (let i = 0; i < items.length; i++) {
+      result += i % 2 == 0 ? items[i] : katex.renderToString(items[i]);
+    }
+    return result;
+  }
   public componentDidMount() {
     MathJax.Hub.Queue(['Typeset', MathJax.Hub]);
   }
@@ -26,7 +39,7 @@ export default class MarkdownRenderer extends React.Component<Props, {}> {
   public render() {
     return (
       <div className='markdown-body'>
-        { renderHTML(marked(this.props.text, { renderer: this.renderer })) }
+        { renderHTML(marked(this.renderKatexFragments(this.props.text), { renderer: this.renderer })) }
       </div>
     );
   }
