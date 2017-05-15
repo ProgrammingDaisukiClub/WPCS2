5ce6da47436b32af546d23d7416a1897ef989287
Shinya Kato <k.shin220@gmail.com>
2017-05-15 12:58:20 +0000

    Improve ranking api

--- app/controllers/api/contests_controller.rb
+++ app/controllers/api/contests_controller.rb
@@ -82,30 +82,98 @@ class Api::ContestsController < ApplicationController
   end
 
   def json_for_ranking
-    {
-      users: sorted_users.map do |user|
-        {
-          id: user.id,
-          name: user.name,
-          total_score: @contest.user_score(user),
-          problems: @contest.problems.includes(:data_sets).map do |problem|
-            {
-              id: problem.id,
-              data_sets: problem.data_sets.order(order: :asc).includes(:submissions).map do |data_set|
-                {
-                  id: data_set.id,
-                  label: data_set.label,
-                  correct: data_set.solved_by_during_contest?(user),
-                  score: data_set.user_score(user),
-                  solved_at: data_set.user_solved_at(user),
-                  wrong_answers: data_set.user_wrong_answers(user)
-                }
-              end
-            }
-          end
+    @users = @contest.users
+    @problems = @contest.problems.includes(:data_sets)
+    @submissions = @contest.submissions.where(created_at: @contest.start_at..@contest.end_at)
+
+    binding.pry
+
+    table = {}
+    @users.map do |user|
+      table[user.id] = {
+        id: user.id,
+        name: user.name,
+        total_score: 0,
+        problems: {}
+      }
+      @problems.map do |problem|
+        table[user.id][:problems][problem.id] = {
+          id: problem.id,
+          data_sets: {}
         }
+        problem.data_sets.map do |data_set|
+          table[user.id][:problems][problem.id][:data_sets][data_set.id] = {
+            id: data_set.id,
+            label: data_set.label,
+            correct: false,
+            score: 0,
+            solved_at: nil,
+            wrong_answers: 0
+          }
+        end
       end
+    end
+
+    @submissions.map do |submission|
+      user_id = submission.user_id
+      problem_id = submission.problem_id
+      data_set_id = submission.data_set_id
+      data = table[user_id][:problems][problem_id][:data_sets][data_set_id]
+
+      if !data[:correct]
+        if submission.judge_status_accepted?
+          table[user_id][:total_score] = table[user_id][:total_score] + submission.score
+          data.merge!(
+            correct: true,
+            score: submission.score,
+            solved_at: submission.created_at
+          )
+        elsif submission.judge_status_wrong?
+          data.merge!(
+            wrong_answers: data[:wrong_answers] + 1
+          )
+        end
+      end
+    end
+
+    @users.map do |user|
+      @problems.map do |problem|
+        data = table[user.id][:problems][problem.id]
+        data[:data_sets] = data[:data_sets].values
+      end
+      data = table[user.id]
+      data[:problems] = data[:problems].values
+    end
+    table = table.values
+
+    {
+      users: table.sort { |user1, user2| user2[:total_score] <=> user1[:total_score] }
     }
+
+    # {
+    #   users: sorted_users.map do |user|
+    #     {
+    #       id: user.id,
+    #       name: user.name,
+    #       total_score: @contest.user_score(user),
+    #       problems: @contest.problems.includes(:data_sets).map do |problem|
+    #         {
+    #           id: problem.id,
+    #           data_sets: problem.data_sets.order(order: :asc).includes(:submissions).map do |data_set|
+    #             {
+    #               id: data_set.id,
+    #               label: data_set.label,
+    #               correct: data_set.solved_by_during_contest?(user),
+    #               score: data_set.user_score(user),
+    #               solved_at: data_set.user_solved_at(user),
+    #               wrong_answers: data_set.user_wrong_answers(user)
+    #             }
+    #           end
+    #         }
+    #       end
+    #     }
+    #   end
+    # }
   end
 
   def sorted_users
