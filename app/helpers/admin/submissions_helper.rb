05038203de5a82d3512b3040f949c7c2c9f0a0bf
Shinya Kato <k.shin220@gmail.com>
2017-05-19 11:28:27 +0000

    Fix submissions index for admins #216

--- app/helpers/admin/submissions_helper.rb
+++ app/helpers/admin/submissions_helper.rb
@@ -1,5 +1,5 @@
 module Admin::SubmissionsHelper
-  JUDGE_STATUS = %w(WJ AC WA).freeze
+  JUDGE_STATUS = %w(WJ WA AC).freeze
 
   def judge_status_badge(submission)
     status_name = JUDGE_STATUS[submission.judge_status_before_type_cast]
