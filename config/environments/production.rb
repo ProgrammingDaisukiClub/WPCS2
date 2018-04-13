07dba6eb4a2d54fc3bf03a9846212eda65855095
getty104 <hayabusatoshihumi@gmail.com>
2018-04-13 20:21:59 +0900

    小文字の修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "wpcs2_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "WPCS2_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['HOST'] }
   ActionMailer::Base.delivery_method = :smtp
