47a7ec99f118174a1339cd53c74c2cead281fc70
getty104 <hayabusatoshihumi@gmail.com>
2018-05-07 15:39:08 +0900

    fix

--- db/migrate/20180507054454_add_status_and_password_to_contests.rb
+++ db/migrate/20180507054454_add_status_and_password_to_contests.rb
@@ -1,6 +1,16 @@
 class AddStatusAndPasswordToContests < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :contests, :password, :string
     add_column :contests, :status, :integer
+
+    Contest.update_all("status=#{Contest.statuses['outside']}, password=''")
+
+    change_column :contests, :password, :string , null: false
+    change_column :contests, :status, :string , null: false
+  end
+
+  def down
+    remove_column :contests, :password
+    remove_column :contests, :status
   end
 end
