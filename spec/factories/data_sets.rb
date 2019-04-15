93d10863fafb8490fe12cf4e02cb3ab941706012
shikugawa <shikugawa@gmail.com>
2019-04-16 01:41:25 +0900

    update gems & modules version

--- spec/factories/data_sets.rb
+++ spec/factories/data_sets.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :data_set do
     sequence(:label) { |n| "label of data_set#{n}" }
-    input 'input of data_set'
-    output 'output of data_set'
+    input { 'input of data_set' }
+    output { 'output of data_set' }
     sequence(:score) { |n| n * 10 }
-    accuracy 0
+    accuracy { 0 }
     sequence(:order) { |n| n }
   end
 end
