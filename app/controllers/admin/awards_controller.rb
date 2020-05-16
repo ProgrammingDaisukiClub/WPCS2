eb004109a525888520b89a21af6e7e23905262d6
Kotaro Inoue <k.musaino@gmail.com>
2020-05-16 11:31:39 +0900

    Remove unused code

--- app/controllers/admin/awards_controller.rb
+++ app/controllers/admin/awards_controller.rb
@@ -24,16 +24,6 @@ class Admin::AwardsController < Admin::ControllerBase
     end
 
     def calculate_fastac_candidates
-      # @users = @contest.users
-      # @problems = @contest.problems.includes(:data_sets).includes(:data_sets)
-      # @submissions = @contest.submissions
-      #                      .where(created_at: @contest.start_at..@contest.end_at)
-      #                      .where(problems: {data_sets: {label: "Large"}})
-      #                      .order(:created_at).includes(:data_set, :problem, :user)
-      # fastac_awards = {}
-      # @problems.map do |problem|
-      # end
-
       response = {}
       con = ActiveRecord::Base.connection
       con.select_all(fastac_query(params[:contest_id])).to_hash.map do |v|
@@ -56,36 +46,6 @@ class Admin::AwardsController < Admin::ControllerBase
       calculate_ranking_score(table)
       table_to_array(table)
     end
-
-    # def ranking_query(contest_id)
-    #   query = <<-QUERY
-    #   SELECT
-    #   users.id AS user_id,
-    #   users.name AS user_name,
-    #   SUM(submissions.score)
-    #   FROM submissions
-    #   JOIN users ON submissions.user_id = users.id
-    #   JOIN data_sets ON submissions.data_set_id = data_sets.id
-    #   JOIN problems ON data_sets.problem_id = problems.id
-    #   JOIN contests ON problems.contest_id = contests.id
-    #   WHERE submissions.created_at >= contests.start_at
-    #       AND submissions.created_at <= contests.end_at
-    #       AND submissions.judge_status = 2
-    #       AND contests.id = ?
-    #       AND submissions.created_at = (
-    #           SELECT MIN(created_at) FROM submissions
-    #           WHERE users.id = submissions.user_id
-    #           AND data_set_id = data_sets.id
-    #           AND submissions.judge_status = 2
-    #       )
-    #   GROUP BY users.id
-    #   ORDER BY SUM(submissions.score) DESC
-    #   LIMIT 50;
-    #   QUERY
-    #   ActiveRecord::Base.send(
-    #     :sanitize_sql_array,
-    #     [query, contest_id])
-    # end
 
   def fastac_query(contest_id)
     query = <<-QUERY
