1c65e705adee5afdae11862f7da893d80921c959
Kotaro Inoue <k.musaino@gmail.com>
2020-05-19 21:25:14 +0900

    Fix to include admin css in precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join("vendor", "assets", "ja
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[build/*.js pages/*.css]
+Rails.application.config.assets.precompile += %w[build/*.js pages/**/*.css]
